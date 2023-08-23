c27905a3f0ac8c38e93b3f1e93ee263974b8473d
manojown <manojown1@gmail.com>
2023-08-23 13:24:52 +0530

    cors fix

--- server/pkg/middleware/fiber.go
+++ server/pkg/middleware/fiber.go
@@ -11,12 +11,7 @@ import (
 func FiberMiddleware(a *fiber.App) {
 	a.Use(
 		// Add CORS to each route.
-		cors.New(cors.Config{
-			AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-			AllowOrigins:     "*",
-			AllowCredentials: true,
-			AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-		}),
+		cors.New(),
 		// Add simple logger.
 		logger.New(),
 	)
